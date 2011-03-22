88409777be6a32bde2a3ad9b33ff7c953586679c
Erik Garrison <erik.garrison@bc.edu>
2011-03-22 15:21:12 -0400

    samplelines, random sampling of lines from file

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: tsvavg tsvsplit samplelines
+all: tsvavg tsvsplit samplelines tsvbootstrap
 
 tsvavg: tsvavg.cpp split.cpp join.h
 	g++ -O3 tsvavg.cpp split.cpp -o tsvavg
 
 tsvsplit: tsvsplit.cpp split.cpp join.h
 	g++ -O3 tsvsplit.cpp split.cpp -o tsvsplit
+
+tsvbootstrap: tsvbootstrap.cpp split.cpp join.h
+	g++ -O3 tsvbootstrap.cpp split.cpp -o tsvbootstrap
 
 samplelines: samplelines.cpp
 	g++ -O3 samplelines.cpp -o samplelines
--- samplelines.cpp
+++ samplelines.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <getopt.h>
 #include <stdlib.h>
 #include <time.h>
 #include "mt19937ar.h"
@@ -11,25 +12,75 @@ using namespace std;
 
 int main (int argc, char** argv) {
 
-    if (argc != 2) {
-        cerr << "usage: " << argv[0] << " sample_rate" << endl
-             << "samples lines from stdin at sample_rate rate" << endl;
+    if (argc < 2) {
+        cerr << "usage: " << argv[0] << " -r sample_rate" << endl
+             << "samples lines from stdin at sample_rate rate" << endl
+             << "specify -H to always print header line" << endl;
         exit(1);
     }
 
-    double sample_rate = atof(argv[1]);
+    double sample_rate = 1.0;
+    bool header = false;
+
+    while (true) {
+        static struct option long_options[] =
+        {  
+            {"header", no_argument, 0, 'H'},
+            {"rate",   required_argument, 0, 'r'},
+
+        };
+        int option_index = 0;
+
+        int c = getopt_long (argc, argv, "Hr:", long_options, &option_index);
+
+          if (c == -1)
+            break;
+
+          switch (c) {
+            case 0:
+            /* If this option set a flag, do nothing else now. */
+            if (long_options[option_index].flag != 0)
+              break;
+            printf ("option %s", long_options[option_index].name);
+            if (optarg)
+              printf (" with arg %s", optarg);
+            printf ("\n");
+            break;
+
+          case 'H':
+            header = true;
+            break;
+
+          case 'r':
+            sample_rate = atof(optarg);
+            break;
+
+          default:
+            abort();
+            break;
+
+        }
+    }
+
 
     // seed prng with random bits from /dev/random
     fstream random;
     random.open("/dev/random", fstream::in);
     int seed;
     random.get((char*) &seed, sizeof(int));
+    random.close();
 
     init_genrand(seed);
 
     string line;
     getline(cin, line);
+    if (header) {
+        cout << line << endl;
+    }
+    getline(cin, line);
+
     while (!line.empty()) {
+        cout << line << endl;
         if (genrand_real1() < sample_rate) {
             cout << line << endl;
         }
